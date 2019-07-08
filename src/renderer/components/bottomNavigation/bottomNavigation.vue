e80efeb48a8534058c4e9938d75bf1bd060c795d
Iroha1024 <47475495+Iroha1024@users.noreply.github.com>
2019-07-08 22:15:30 +0800

    底部导航栏

--- src/renderer/components/bottomNavigation/bottomNavigation.vue
+++ src/renderer/components/bottomNavigation/bottomNavigation.vue
@@ -1,30 +1,80 @@
 <template>
     <div id="bottom-nav">
-        <router-link to="/home" tag="div" class="iconfont">&#xe600;</router-link>
-        <router-link to="/follow" tag="div" class="iconfont">&#xe607;</router-link>
-        <router-link to="/message" tag="div" class="iconfont">&#xe60d;</router-link>
-        <router-link to="/info" tag="div" class="iconfont">&#xe608;</router-link>
+        <router-link tag="div" :to="item.url" v-for="(item, index) of link" :key="index"
+            class="iconfont" :class="{hover: !item.activate}" @click.native="toggleBgColor(index)">
+            <div :class="{clicked: item.activate}">
+                {{ item.icon }}
+            </div>
+        </router-link>  
     </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            link: [
+                {
+                    url: '/home',
+                    icon: '\ue600',
+                    activate: true
+                },
+                {
+                    url: '/follow',
+                    icon: '\ue607',
+                    activate: false
+                },
+                {
+                    url: '/message',
+                    icon: '\ue60d',
+                    activate: false
+                },
+                {
+                    url: '/info',
+                    icon: '\ue608',
+                    activate: false
+                },
+            ]
+        }
+    },
+    methods: {
+        //路由切换时，改变对应背景颜色
+        toggleBgColor(index) {
+            this.link.forEach((item) => {
+                item.activate = false;
+            })
+            this.link[index].activate = true;
+        }
+    },
 }
 </script>
 
 <style lang="scss" scoped>
+@import '~css/global';
     #bottom-nav {
-            position: absolute;
-            bottom: 0;
-            margin-bottom: 10px;
-            width: 100%;
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        //消除display带来的间隙
+        font-size:0;
         div {
-            float: left;
+            display: inline-block;
             width: 25%;
+            border-radius: 15px 15px 0 0;
             text-align: center;
-            font-size: 25px;
+            font-size: $bnHeight * 0.625;
+            height: $bnHeight;
+            line-height: $bnHeight;
+            cursor: pointer;
+        }
+        .hover:hover {
+            background: $hoverColor;
+        }
+        .clicked {
+            width: $bnHeight;
+            height: $bnHeight;
+            border-radius: $bnHeight / 2;
+            background: $clickedColor;
         }
     }
-        
 </style>
